25de410c513e2d2af325e44e874146fee30a98b6
zidtalel <33374909+zidtalel@users.noreply.github.com>
2023-01-04 21:31:24 -0500

    Update Calculator.java

--- JavaSonar/src/main/java/main/Calculator.java
+++ JavaSonar/src/main/java/main/Calculator.java
@@ -4,5 +4,5 @@ public interface Calculator {
 	int multiply(int a, int b); 
 	int divide(int a, int b); 
 	int add(int a, int b); 
-	int substract(int a, int b); 
+	int substract(int a, int b);  
 }
